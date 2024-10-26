5ea2c74235623b971623996caa3367ba1cd76fe3
DESKTOP-6RHFIOS\jiyao <jiyaoskywalker@163.com>
2024-10-27 01:17:59 +0800

    fix: Unit脚本

--- Assets/Scripts/Units/InteractiveItem/Chest.cs
+++ Assets/Scripts/Units/InteractiveItem/Chest.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TbsFramework.Units;
 using UnityEngine;
 
-public class Chest : MonoBehaviour
+public class Chest : Unit
 {
     // Start is called before the first frame update
     void Start()
