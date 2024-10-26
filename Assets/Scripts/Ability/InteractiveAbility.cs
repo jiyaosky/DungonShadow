fd97389e116206db666009e7de199eedf537be58
DESKTOP-6RHFIOS\jiyao <jiyaoskywalker@163.com>
2024-10-27 01:04:33 +0800

    fix: 增加新的Unit，宝箱

--- Assets/Scripts/Ability/InteractiveAbility.cs
+++ Assets/Scripts/Ability/InteractiveAbility.cs
@@ -1,45 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TbsFramework.Cells;
 using TbsFramework.Grid;
 using UnityEngine;
 
 namespace TbsFramework.Units.Abilities
 {
-    public class InteractiveAbility : PlayerAttackAbility
+    public class InteractiveAbility : Ability
     {
+        bool isInteracted = false;
+        private Cell currentCell;
         public int Range = 1;
         private void Start()
         {
+            currentCell = GetComponent<Cell>();
+            isInteracted = false;
         }
+        
+        
 
-        private void Update()
+        public override IEnumerator Act(CellGrid cellGrid, bool isNetworkInvoked = false)
         {
+            var myUnits = cellGrid.GetCurrentPlayerUnits();
+            var unitsInRange = myUnits.Where(u => currentCell.GetDistance(UnitReference.Cell) <= Range);
 
+            foreach (var unit in unitsInRange)
+            {
+                // TODO:当玩家进入这个范围的时候可以做些什么（例如提示 or UI显示）
+                Debug.Log("Unit in range");
+                // 先设置为可交互状态吧
+                isInteracted = true;
+            }
+
+            yield return null;
         }
-
-        // public override IEnumerator Act(CellGrid cellGrid, bool isNetworkInvoked = false)
-        // {
-        //     var myUnits = cellGrid.GetCurrentPlayerUnits();
-        //     var unitsInRange = myUnits.Where(u => u.Cell.GetDistance(UnitReference.Cell) <= Range);
-
-        //     foreach (var unit in unitsInRange)
-        //     {
-        //         if (unit.Equals(UnitReference) && !ApplyToSelf)
-        //         {
-        //             continue;
-        //         }
-        //         unit.AddBuff(Buff);
-        //     }
-
-        //     yield return null;
-        // }
 
 
         public virtual void InterPlay()
         {
-
+            if (isInteracted)
+            {
+                // TODO:当玩家点击这个单元格的时候与其交互吧
+                Debug.Log("Interacted");
+            }
         }
-
     }
 }
