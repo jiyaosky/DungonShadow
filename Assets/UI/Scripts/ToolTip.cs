c3fe9a7e98ed957eb82b18a159ef97a9e960fd6a
CHN\zihan.guo <zihan.guo@garena.com>
2024-10-29 19:17:46 +0800

    增加Tooltips脚本

---
+++ Assets/UI/Scripts/ToolTip.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToolTip : MonoBehaviour, IPointerEnterHandler
+{
+    private GameObject dialog_box;
+
+    void Start()
+    {
+        dialog_box = this.gameObject.transform.FindChild("DialogBox").gameObject;
+        Debug.Log("Start");
+    }
+    
+
+    //if (dialog_box == null) { Debug.Log("No child fopund"); }
+        
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        dialog_box.SetActive(true);
+        //Debug.Log("The cursor entered the selectable UI element.");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        dialog_box.SetActive(false);
+        //Debug.Log("The cursor entered the selectable UI element.");
+    }
+
+
+
+}
