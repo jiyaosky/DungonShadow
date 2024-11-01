22237edf0fc63ae9468e3515452ae1d7a4d47915
CHN\zihan.guo <zihan.guo@garena.com>
2024-11-01 15:50:40 +0800

    修了点BUG

--- Assets/UI/Scripts/ToolTip.cs
+++ Assets/UI/Scripts/ToolTip.cs
@@ -20,8 +20,6 @@ public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     [SerializeField]
     public TextMeshProUGUI CD_info;
 
-    public SkillScript skillScript;
-
     public int AP;
     public int CD;
 
@@ -30,18 +28,16 @@ public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     void Start()
     {
         //Set UI Position
-        dialog_box.transform.position = new Vector3(this.gameObject.transform.position.x + 60, this.gameObject.transform.position.y + 60, this.gameObject.transform.position.z);
+        dialog_box.transform.position = new Vector3(this.gameObject.transform.position.x + 70, this.gameObject.transform.position.y + 60, this.gameObject.transform.position.z);
 
         AP_panel1.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y -25, this.gameObject.transform.position.z);
         AP_info.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - 25, this.gameObject.transform.position.z);
         CD_panle2.transform.position = new Vector3(this.gameObject.transform.position.x + 20, this.gameObject.transform.position.y + 30, this.gameObject.transform.position.z);
         CD_info.transform.position = new Vector3(this.gameObject.transform.position.x + 20, this.gameObject.transform.position.y + 30, this.gameObject.transform.position.z);
 
-
         //Read AP and CD
         //AP = skill.GetComponent<ImmobilizationSkill>().APCost;
         //CD = skill.GetComponent<ImmobilizationSkill>().CoolDown;
-
 
     }
     
