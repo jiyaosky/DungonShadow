f52b1db5683f5a55e071a205b52a96ad7fbd2156
DESKTOP-6RHFIOS\jiyao <jiyaoskywalker@163.com>
2024-11-03 01:55:08 +0800

    增加game场景

--- Assets/Scripts/Utils/LevelManager.cs
+++ Assets/Scripts/Utils/LevelManager.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using RTS_Cam;
+using TbsFramework.Grid;
+using TbsFramework.Units;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
     public List<GameObject> levels;
+    public RTS_Camera RTSCamera;
+    public Unit currentPlayer;
 
-    public void InstantiateLevel()
+    public int weaponIndex;
+
+    
+    public CellGrid currentCellGrid;
+    public void InstantiateLevel(int weapon)
     {
+        // 先记住自己的武器吧
+        weaponIndex = weapon;
+        // 启动关卡
         levels[0].SetActive(true);
+        
+        // 找到CellGrid
+        currentCellGrid = levels[0].GetComponentInChildren<CellGrid>();
+        // 开始游戏吧
+        currentCellGrid.InitializeAndStart();
+        currentPlayer = currentCellGrid.AIGetEnemyUnits()[0];
+        
+        // 设置RTSCamera
+        RTSCamera = FindObjectOfType<RTS_Camera>();
+        RTSCamera.SetTarget(currentPlayer.transform);
+
+        // 设置Fog
+
+
+
     }
     
     public void NextLevel()
