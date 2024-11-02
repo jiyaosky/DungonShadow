54a37efa861e9f4cc501b758b3c0964b440d6639
DESKTOP-39EODQS\ggggg <guo329purdue@gmail.com>
2024-11-02 17:59:35 +0800

    基础行为UI接入

---
+++ Assets/UI/Scripts/ToolTip_Basic.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TbsFramework;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ToolTip_Basic: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+
+    [SerializeField]
+    private GameObject dialog_box;
+
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        dialog_box.SetActive(true);
+        //Debug.Log("The cursor entered the selectable UI element.");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        dialog_box.SetActive(false);
+        //Debug.Log("The cursor entered the selectable UI element.");
+    }
+
+}
